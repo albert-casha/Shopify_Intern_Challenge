a1d09b81ab46a69181d10df7259f811f8c00ea41
albert-casha <90425390+albert-casha@users.noreply.github.com>
2021-09-10 09:38:23 -0400

    Update Question2a.sql

--- Question2a.sql
+++ Question2a.sql
@@ -1,6 +1,6 @@
 SELECT shippers.ShipperName, 
 		COUNT(ShipperName) as Number_of_orders
-FROM Shippers as shippers
-JOIN Orders as orders
-ON shippers.ShipperID = orders.ShipperID
+FROM Shippers
+JOIN Orders
+ON Shippers.ShipperID = Orders.ShipperID
 GROUP BY ShipperName
