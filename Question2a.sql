434236a039b07b91dd4c311b998ff8176258a168
albert-casha <90425390+albert-casha@users.noreply.github.com>
2021-09-10 09:40:37 -0400

    Update Question2a.sql

--- Question2a.sql
+++ Question2a.sql
@@ -1,6 +1,7 @@
-SELECT shippers.ShipperName, 
-		COUNT(ShipperName) as Number_of_orders
+SELECT Shippers.ShipperName, 
+	   COUNT(ShipperName) as Number_of_orders
 FROM Shippers
 JOIN Orders
 ON Shippers.ShipperID = Orders.ShipperID
+WHERE Shippers.ShipperName = 'Speedy Express'
 GROUP BY ShipperName
