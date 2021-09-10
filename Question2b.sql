d242b028412bbcec6e75ff46ba5205b8e67acf04
albert-casha <90425390+albert-casha@users.noreply.github.com>
2021-09-10 09:45:46 -0400

    Update Question2b.sql

--- Question2b.sql
+++ Question2b.sql
@@ -1,8 +1,8 @@
-SELECT employees.LastName, 
-	   employees.FirstName, 
-       COUNT(orders.OrderID) as Number_of_Orders,
-FROM Orders as orders
-JOIN Employees as employees
-ON orders.EmployeeID = employees.EmployeeID
-GROUP BY employees.LastName, employees.FirstName
+SELECT Employees.LastName, 
+       Employees.FirstName, 
+       COUNT(Orders.OrderID) as Number_of_Orders
+FROM Orders
+JOIN Employees
+ON Orders.EmployeeID = Employees.EmployeeID
+GROUP BY Employees.LastName, Employees.FirstName
 ORDER BY Number_of_Orders DESC
